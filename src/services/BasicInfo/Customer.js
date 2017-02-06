729fb4182d229a4f880e24c6a2cc1e4b741ef735
yangwenzhu <ywz19950420@163.com>
2017-02-06 10:44:57 +0800

    customer service

--- src/services/BasicInfo/Customer.js
+++ src/services/BasicInfo/Customer.js
@@ -1,11 +1,48 @@
 import request from '../../utils/request';
 import qs from 'qs';
 
+
 export async function queryCustomer(params) {
-    console.log('customer service');
-    return request('/api/customer/query', {
+    return request(`/api/customer/query?${qs.stringify(params)}`);
+}
+
+export async function create(params) {
+    params.customer = null;
+    return request(`/api/customer/insert`, {
         method: 'post',
-        body: qs.stringify(params),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params),
+    })
+}
+
+export async function get(params) {
+    return request(`/api/customer/get?${qs.stringify(params)}`);
+}
+
+export async function remove(params) {
+    return request(`/api/customer/remove?${qs.stringify(params)}`,{
+        method:'put',
     });
 }
+
+export async function recover(params) {
+    return request(`/api/customer/recover?${qs.stringify(params)}`,{
+        method:'put',
+    });
+}
+
+export async function update(params) {
+  return request('/api/customer/update', {
+    method: 'put',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(params),
+  });
+}
+
 
