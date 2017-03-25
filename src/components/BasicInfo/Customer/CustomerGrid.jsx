eb64aa3826967d9f0e1aa594a3af86723c7ba9a2
yangwenzhu <ywz19950420@163.com>
2017-03-25 18:31:03 +0800

    customergrid重构

--- src/components/BasicInfo/Customer/CustomerGrid.jsx
+++ src/components/BasicInfo/Customer/CustomerGrid.jsx
@@ -1,124 +1,141 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Table, message, Popconfirm, Button, Row, Col, Card, Spin } from 'antd';
-import style from './CustomerGrid.less';
 
-function CustomerGrid({
-    dataSource,
-    onSearch,
-    onCreate,
-    pagination,
-    onPageChange,
-    onViewItem,
-    onEdit,
-    onDelete,
-    onRecover,
-    onRemoveBatch,
-    onRecoverBatch,
-    customers = [],
-}) {
 
-    const columns = [{
-        title: '代码',
-        dataIndex: 'code',
-        key: 'code',
-        render: (text, record) => <a onClick={() => { onViewItem(record) }}>{text}</a>,
-        sorter: true,
-    },
-    {
-        title: '名称',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: true,
-    },
-    {
-        title: '状态',
-        dataIndex: 'state',
-        key: 'state',
-    },
-    {
-        title: '操作',
-        key: 'operation',
-        render: (text, record) => (
-            <p>
-                <a onClick={() => { onEdit(record) }}>编辑</a>
-                &nbsp;
+const columns = [{
+    title: '代码',
+    dataIndex: 'code',
+    key: 'code',
+    render: (text, record) => <a onClick={() => { onViewItem(record) }}>{text}</a>,
+    sorter: true,
+},
+{
+    title: '名称',
+    dataIndex: 'name',
+    key: 'name',
+    sorter: true,
+},
+{
+    title: '状态',
+    dataIndex: 'state',
+    key: 'state',
+},
+{
+    title: '操作',
+    key: 'operation',
+    render: (text, record) => (
+        <p>
+            <a onClick={() => { onEdit(record) }}>编辑</a>
+            &nbsp;
                 <Popconfirm title="确定要删除吗？" onConfirm={() => onDelete(record)} >
-                    <a disabled={record.state === "已删除"}>删除</a>
-                </Popconfirm>
-                <Popconfirm title="确定要恢复吗？" onConfirm={() => onRecover(record)}>
-                    <a disabled={record.state === "正常"}>恢复</a>
-                </Popconfirm>
-            </p>
-        )
-    }
-    ];
+                <a disabled={record.state === "已删除"}>删除</a>
+            </Popconfirm>
+            <Popconfirm title="确定要恢复吗？" onConfirm={() => onRecover(record)}>
+                <a disabled={record.state === "正常"}>恢复</a>
+            </Popconfirm>
+        </p>
+    )
+}
+];
 
-    const rowSelection = {
-        onChange: (selectedRowKeys, selectedRows) => {
-            console.log('...测试行选择器onchange');
-
-        },
-        onSelect: (record, selected, selectedRows) => {
-            console.log('...测试行选择器onselect');
-            if (customers.length === 0) {
-                customers = selectedRows;
-            } else {
-                selected = false;
-                selectedRows = null;
-            }
-        },
-        onSelectAll: (selected, selectedRows, changeRows) => {
-            console.log('...测试行选择器onselectall');
-            console.log(customers.length);
-            if (customers.length === 0) {
-                console.log('customers 的长度');
-                customers = selectedRows;
-            } else {
-                selected = false;
-            }
-        },
-        getCheckboxProps: record => ({
-            disabled: record.name === 'Disabled User',
-        }),
+class CustomerGrid extends React.Component {
+    constructor(props) {
+        console.log("初始化");
+        console.dir(props);
+        super(props);
+        this.state = {
+            ...props,
+            selectedRowKeys: [],
+        }
+        this.handleRemoveBatch = this.handleRemoveBatch.bind(this);
+        this.handleRecoverBatch = this.handleRecoverBatch.bind(this);
+    };
+    componentWillReceiveProps(newProps) {
+        this.setState({
+            ...newProps,
+            selectedRowKeys: [],
+        });
+    };
+    onSelectChange = (selectedRowKeys) => {
+        this.setState({ selectedRowKeys });
+    };
+    handleRemoveBatch() {
+        this.state.onRemoveBatch(this.state.selectedRowKeys);
+        // this.setState({ selectedRowKeys: [] });
+    };
+    handleRecoverBatch() {
+        this.state.onRecoverBatch(this.state.selectedRowKeys);
     };
 
-    function handleRemoveBatch() {
-        onRemoveBatch(customers);
-    }
-
-    function handleRecoverBatch() {
-        onRecoverBatch(customers);
-    }
-
-    return (
-        <div>
-                <Table className={style.table}
+    render() {
+        const columns = [{
+            title: '代码',
+            dataIndex: 'code',
+            key: 'code',
+            render: (text, record) => <a onClick={() => { this.state.onViewItem(record) }}>{text}</a>,
+            sorter: true,
+        },
+        {
+            title: '名称',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: true,
+        },
+        {
+            title: '状态',
+            dataIndex: 'state',
+            key: 'state',
+        },
+        {
+            title: '操作',
+            key: 'operation',
+            render: (text, record) => (
+                <p>
+                    <a onClick={() => { this.state.onEdit(record) }}>编辑</a>
+                    &nbsp;
+                <Popconfirm title="确定要删除吗？" onConfirm={() => this.state.onDelete(record)} >
+                        <a disabled={record.state === "已删除"}>删除</a>
+                    </Popconfirm>
+                    <Popconfirm title="确定要恢复吗？" onConfirm={() => this.state.onRecover(record)}>
+                        <a disabled={record.state === "正常"}>恢复</a>
+                    </Popconfirm>
+                </p>
+            )
+        }
+        ];
+        const { selectedRowKeys } = this.state;
+        const rowSelection = {
+            selectedRowKeys,
+            onChange: this.onSelectChange,
+        };
+        const hasSelected = selectedRowKeys.length > 0;
+        return (
+            <div>
+                <Table
                     size="small"
                     columns={columns}
-                    dataSource={dataSource}
+                    dataSource={this.state.dataSource}
                     rowKey={record => record.uuid}
-                    onChange={onPageChange}
-                    pagination={pagination}
+                    onChange={this.state.onPageChange}
+                    pagination={this.state.pagination}
                     bordered
                     rowSelection={rowSelection}
                     title={
                         () =>
                             <div>
                                 <Row type="flex">
-                                    <Col><Button type="ghost" onClick={handleRemoveBatch}>批量删除</Button></Col>
-                                    <Col><Button type="ghost" onClick={handleRecoverBatch}>批量恢复</Button></Col>
-                                    <Col><Button onClick={() => onCreate()}>新建</Button></Col>
+                                    <Col><Button type="ghost" onClick={this.handleRemoveBatch}>批量删除</Button></Col>
+                                    <Col><Button type="ghost" onClick={this.handleRecoverBatch}>批量恢复</Button></Col>
+                                    <Col><Button onClick={this.state.onCreate}>新建</Button></Col>
+                                    <Col><span style={{ marginLeft: 8 }}>{hasSelected ? `已选中${selectedRowKeys.length}条` : ''}</span></Col>
                                 </Row>
                             </div>
 
                     }
                 />
-        </div>
-    );
-}
-
-CustomerGrid.propTypes = {
-    onPageChange: PropTypes.func,
+            </div>
+        );
+    }
 }
 
 export default CustomerGrid;
