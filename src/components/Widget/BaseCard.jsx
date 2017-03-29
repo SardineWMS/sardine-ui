e0414e7a18eae3d6710f6b8af91247ca722ca88e
zhangsai02 <674016520@qq.com>
2017-03-29 16:25:02 +0800

    BaseCard.jsx

--- src/components/Widget/BaseCard.jsx
+++ src/components/Widget/BaseCard.jsx
@@ -3,7 +3,7 @@ import {Form,Button,Card,Row,Col} from 'antd';
 import Guid from '../../utils/Guid';
 const FormItem = Form.Item;
 
-
+/** 基本卡片，带标题，可单列也可两列，暂不支持超过两列，两列时设置single={true}即可 **/
 class BaseCard extends React.Component{
 
   render(){
