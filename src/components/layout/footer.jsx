1199b5a79bee83ac1196592e2256c8717e82f7f0
wujing509@163.com <wujing509@163.com>
2017-05-16 10:50:52 +0800

    更名

--- src/components/layout/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react'
-import styles from '../less/main.less'
-import { config } from '../../utils'
-
-const Footer = () => <div className={styles.footer}>
-  {config.footerText}
-</div>
-
-export default Footer
