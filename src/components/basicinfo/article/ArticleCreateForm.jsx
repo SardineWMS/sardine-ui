4bfc2bef441bd4ca5f7e9b7ff2b3bf1f26c26ecf
wujing509@163.com <wujing509@163.com>
2017-05-16 10:48:26 +0800

    更名

--- src/components/basicinfo/article/ArticleCreateForm.jsx
+++ src/components/basicinfo/article/ArticleCreateForm.jsx
@@ -108,7 +108,7 @@ const ArticleCreateForm = ({
 
   children.push(
     <BaseFormItem label="商品类别 :">
-      {getFieldDecorator('categoryCode', {
+      {getFieldDecorator('category', {
                   initialValue: article ? article.category : null,
                   rules: [
                     { required: true, message: '类别未填写' },
