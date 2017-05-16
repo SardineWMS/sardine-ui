e304047a75cf823878162964fee5aa55f47ba8a6
wujing509@163.com <wujing509@163.com>
2017-05-16 11:08:47 +0800

    更名

--- src/services/inner/Task.js
+++
@@ -1,8 +0,0 @@
-import request from '../../utils/request';
-import qs from 'qs';
-import { query, addTokenToUrl, createBase, updateBase, deleteBase } from '../../utils/BaseService';
-
-export async function queryTask(params) {
-    const url = "/inner/task/query";
-    return request(query(url, params));
-}
--- src/services/log/EntityLog.js
+++
@@ -1,6 +0,0 @@
-import request from '../../utils/request';
-import { query} from '../../utils/BaseService.js';
-export async function queryEntityLogs(params) {
-  const url = "/Log/entitylog/querybypage";
-  return request(query(url, params));
-}
