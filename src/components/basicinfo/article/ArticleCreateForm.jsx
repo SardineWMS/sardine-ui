5dee039675b51d2c597a4643a4097c98e4295adf
zhangsai02 <674016520@qq.com>
2017-06-21 15:11:04 +0800

    测试

--- src/components/basicinfo/article/ArticleCreateForm.jsx
+++ src/components/basicinfo/article/ArticleCreateForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import {
   Form, Row, Col, Input, Button, Icon, Table, message, Popconfirm, Card, Select, InputNumber
   , Radio
@@ -12,7 +12,6 @@ import RemarkCard from '../../Widget/RemarkCard';
 import Panel from '../../Widget/Panel';
 
 const RadioGroup = Radio.Group;
-
 
 const ArticleCreateForm = ({
   article = {},
@@ -35,13 +34,13 @@ const ArticleCreateForm = ({
       if (errors) {
         return;
       }
-      const data = { ...getFieldsValue(), uuid: article.uuid, version: article.version, category: category };
+      const data = {...getFieldsValue(), uuid: article.uuid, version: article.version, category: category};
       onOk(data);
     });
   }
 
-  function selectCategory(){
-    let data = { ...getFieldsValue()};
+  function selectCategory() {
+    let data = {...getFieldsValue()};
     if (article.uuid) {
       data.uuid = article.uuid;
       data.version = article.version;
@@ -52,9 +51,9 @@ const ArticleCreateForm = ({
   function handleEnterPress() {
     if (category && ("[" + category.code + "]" + category.name) == getFieldsValue().category)
       return;
-    if (!getFieldsValue().category) 
+    if (!getFieldsValue().category)
       return;
-    let data = { ...getFieldsValue()};
+    let data = {...getFieldsValue()};
     if (article.uuid) {
       data.uuid = article.uuid;
       data.version = article.version;
@@ -62,7 +61,7 @@ const ArticleCreateForm = ({
     onEnterCategory(data, getFieldsValue().category);
   }
 
-  function firstInFirstOutChange(e){
+  function firstInFirstOutChange(e) {
     onFirstInFirstOutChange(e.target.value);
   }
 
@@ -73,11 +72,11 @@ const ArticleCreateForm = ({
       {getFieldDecorator('code', {
         initialValue: article ? article.code : null,
         rules: [
-          { required: true, message: '商品代码不能为空！' },
+          {required: true, message: '商品代码不能为空！'},
         ],
       })(
-        <Input type="text" />
-        )}
+        <Input type="text"/>
+      )}
     </BaseFormItem>
   );
 
@@ -86,11 +85,11 @@ const ArticleCreateForm = ({
       {getFieldDecorator('name', {
         initialValue: article ? article.name : null,
         rules: [
-          { required: true, message: '商品名称不能为空！' },
+          {required: true, message: '商品名称不能为空！'},
         ],
       })(
-        <Input type="text" />
-        )}
+        <Input type="text"/>
+      )}
     </BaseFormItem>
   );
 
@@ -99,19 +98,23 @@ const ArticleCreateForm = ({
       {getFieldDecorator('spec', {
         initialValue: article ? article.spec : null,
         rules: [
-          { required: true, message: '包装不能为空！' },
+          {required: true, message: '包装不能为空！'},
         ],
       })(
-        <Input type="text" />
-        )}
+        <Input type="text"/>
+      )}
     </BaseFormItem>
   );
 
   basicChildren.push(<BaseFormItem label={"商品类别："} key="category">
-        {getFieldDecorator("category", { rules: [{ required: true, message: '商品类别不能为空！' }], initialValue: (category && category.uuid) ? "[" + category.code + "]" + category.name : null })(
-            <Input placeholder="请选择" suffix={<Button type="primary" icon="credit-card" onClick={() => selectCategory()} />} onBlur={handleEnterPress} onPressEnter={handleEnterPress} />
-        )}
-    </BaseFormItem>);
+    {getFieldDecorator("category", {
+      rules: [{required: true, message: '商品类别不能为空！'}],
+      initialValue: (category && category.uuid) ? "[" + category.code + "]" + category.name : null
+    })(
+      <Input placeholder="请选择" suffix={<Button type="primary" icon="credit-card" onClick={() => selectCategory()}/>}
+             onBlur={handleEnterPress} onPressEnter={handleEnterPress}/>
+    )}
+  </BaseFormItem>);
 
   basicChildren.push(
     <BaseFormItem label="质量管理 :" key="firstInFirstOut">
@@ -122,7 +125,7 @@ const ArticleCreateForm = ({
           <Radio value={true}>是</Radio>
           <Radio value={false}>否</Radio>
         </Radio.Group>
-        )}
+      )}
     </BaseFormItem>
   );
 
@@ -130,25 +133,25 @@ const ArticleCreateForm = ({
     <BaseFormItem label="保质期 ：" key="expDays">
       {getFieldDecorator('expDays', {
         initialValue: article.expDays ? article.expDays : 0,
-        rules: [{ required: true, message: '保质期不能为空！' },],
+        rules: [{required: true, message: '保质期不能为空！'},],
       })(
-        <InputNumber min={0} />
-        )}
+        <InputNumber min={0}/>
+      )}
     </BaseFormItem>
   );
 
   businessChildren.push(
-    <BaseFormItem label="上架货位 ：" key="putawayBin" >
+    <BaseFormItem label="上架货位 ：" key="putawayBin">
       {getFieldDecorator('putawayBin', {
         initialValue: article.putawayBin ? article.putawayBin : "StorageBin",
-        rules: [{ required: true, message: '上架货位不能为空！' },],
+        rules: [{required: true, message: '上架货位不能为空！'},],
       })(
         <Select size="large">
-            <Select.Option value="StorageBin" >存储位</Select.Option>
-            <Select.Option value="PickUpBin" >拣货位</Select.Option>
-            <Select.Option value="PreChoosePickUp" >优先考虑拣货位</Select.Option>
-        </Select> 
-        )}
+          <Select.Option value="StorageBin">存储位</Select.Option>
+          <Select.Option value="PickUpBin">拣货位</Select.Option>
+          <Select.Option value="PreChoosePickUp">优先考虑拣货位</Select.Option>
+        </Select>
+      )}
     </BaseFormItem>
   );
 
@@ -158,19 +161,19 @@ const ArticleCreateForm = ({
 
   return (
     <div>
-      <ToolbarPanel children={toolbar} />
+      <ToolbarPanel children={toolbar}/>
       <BaseCard title="基本信息">
-        <BaseForm items={basicChildren} />
-        <BaseForm items={businessChildren} />
+        <BaseForm items={basicChildren}/>
+        <BaseForm items={businessChildren}/>
       </BaseCard>
       <Panel title="说明">
-         
-         {getFieldDecorator('remark', {
-        initialValue: article.remark
-      })(
-         <Input type="textarea" autosize={{ minRows: 4 }} defaultValue={article.remark} />
+
+        {getFieldDecorator('remark', {
+          initialValue: article.remark
+        })(
+          <Input type="textarea" autosize={{minRows: 4}} defaultValue={article.remark}/>
         )}
-       </Panel>
+      </Panel>
     </div>
   );
 };
