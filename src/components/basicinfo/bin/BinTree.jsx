f8ba255a42bab115907e0f096ba49f428fb13b66
zhangsai02 <674016520@qq.com>
2017-06-22 10:22:43 +0800

    ZWMS-6680

--- src/components/basicinfo/bin/BinTree.jsx
+++ src/components/basicinfo/bin/BinTree.jsx
@@ -4,34 +4,6 @@ import React, { Component, PropTypes } from 'react';
 const TreeNode = Tree.TreeNode;
 const Search = Input.Search;
 
-const x = 3;
-const y = 2;
-const z = 1;
-const gData = [];
-
-const generateData = (_level, _preKey, _tns) => {
-  const preKey = _preKey || '0';
-  const tns = _tns || gData;
-
-  const children = [];
-  for (let i = 0; i < x; i++) {
-    const key = `${preKey}-${i}`;
-    tns.push({ title: key, key });
-    if (i < y) {
-      children.push(key);
-    };
-  };
-  if (_level < 0) {
-    return tns;
-  };
-  const level = _level - 1;
-  children.forEach((key, index) => {
-    tns[index].children = [];
-    return generateData(level, key, tns[index].children);
-  });
-};
-generateData(z);
-
 const dataList = [];
 const generateList = (data) => {
   for (let i = 0; i < data.length; i++) {
@@ -43,7 +15,6 @@ const generateList = (data) => {
     };
   };
 };
-generateList(gData);
 
 const getParentKey = (key, tree) => {
   let parentKey;
@@ -82,10 +53,11 @@ class BinTree extends React.Component {
     });
   };
   onChange = (e) => {
+    generateList(this.state.data);
     const value = e.target.value;
     const expandedKeys = dataList.map((item) => {
       if (item.key.indexOf(value) > -1) {
-        return getParentKey(item.key, gData);
+        return getParentKey(item.key, this.state.data);
       };
       return null;
     }).filter((item, i, self) => item && self.indexOf(item) === i);
