44c077c13c9c246c1cca598fe3719a50402d8630
zhangsai02 <674016520@qq.com>
2017-08-15 08:42:41 +0800

    打印

--- src/utils/PrintUtil.js
+++ src/utils/PrintUtil.js
@@ -1,3 +1,4 @@
+import React, { Component, PropTypes } from 'react';
 /**
  * 打印预览
  * 通过重定向URL并打开新窗口实现待打印数据的预览
@@ -5,8 +6,8 @@
  **/
 export function printPreview(reportletName, params) {
 	localStorage.setItem('report-url', 'http://localhost:8080/sardine-wms-report');
-    var token = localStorage.getItem("token");
-    var url = localStorage.getItem("report-url") + "/ReportServer?reportlet=" + reportletName + "&token=" + token;
+    var companyUuid = localStorage.getItem("companyUuid");
+    var url = localStorage.getItem("report-url") + "/ReportServer?reportlet=" + reportletName + "&companyUuid=" + companyUuid;
     if (params) {
        params.forEach(function (value, key, map) {
          url = url + "&" + key + "=" + value;
@@ -17,15 +18,12 @@ export function printPreview(reportletName, params) {
 
 export function print(reportletName, params) {
 	localStorage.setItem('report-url', 'http://localhost:8080/sardine-wms-report');
-    var token = localStorage.getItem("token");
-    var url = localStorage.getItem("report-url") + "/ReportServer?reportlet=" + reportletName + "&token=" + token;
+    var companyUuid = localStorage.getItem("companyUuid");
+    var url = localStorage.getItem("report-url") + "/ReportServer?reportlet=" + reportletName + "&companyUuid=" + companyUuid;
     if (params) {
        params.forEach(function (value, key, map) {
          url = url + "&" + key + "=" + value;
        });
     }
-    <script language="javascript">
-       document.write("<script src='test.js'></script>");
-    </script>
-    FR.doURLFlashPrint(url, false);
+    FR.doURLFlashPrint("http://127.0.0.1:8080/sardine-wms-report/ReportServer?reportlet=%E6%B5%8B%E8%AF%95.cpt&companyUuid=xxxx0002&token=null&code=xxx&name=xxx", false);
 }
